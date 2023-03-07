5d0af8a2f7dc0fda36dce19e5e32b4c6947e7468
ykoga <yokoga@gmail.com>
2023-03-08 08:55:20 +0900

    1st commmit

---
+++ sample.py
@@ -0,0 +1,14 @@
+#必要なモジュールのインポート
+from flask import Flask
+
+#　Flask をインスタンス化
+app = Flask(__name__)
+
+# ルートディレクトリにアクセスがあった時の処理
+@app.route('/') 
+def hello():
+    return 'Hello World!'
+
+# エントリーポイント
+if __name__ == '__main__':
+    app.run()
---
+++ src/view.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+#　必要なモジュールのインポート
+from flask import Flask, render_template
+
+# app という変数でFlaskオブジェクトをインスタンス化
+app = Flask(__name__)
+
+# --- View側の設定 ---
+# ルートディレクトリにアクセスした場合の挙動
+@app.route('/')
+# def以下がアクセス後の操作
+def index():
+    return render_template('index.html') #追加
+
+if __name__ == '__main__':
+    app.run()
