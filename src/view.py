ca45aa1a8b2d1f75105217113967431526db3db9
ykoga <yokoga@gmail.com>
2023-03-08 14:04:38 +0900

    1st commmit

--- src/view.py
+++ src/view.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 #　必要なモジュールのインポート
-from flask import Flask, render_template, make_response, request
+from flask import Flask, render_template, make_response, request, jsonify
 
 # app という変数でFlaskオブジェクトをインスタンス化
 app = Flask(__name__)
@@ -10,9 +10,13 @@ app = Flask(__name__)
 def index():
     #return '3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P'
     #return render_template('index.html') #追加
-    response = make_response('3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P')
-    response.headers['Content-type'] = 'text/plain'
-    return response
+    #response = make_response('3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P')
+    #response.headers['Content-type'] = 'text/plain'
+    #return response
+    out = {
+        'challenge':'3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P'
+    }
+    return jsonify(out)
 
 if __name__ == '__main__':
     app.run(debug=True)
