1ddcaa80d980ef43f95c6512b2cf2e9c28c5d8c5
ykoga <yokoga@gmail.com>
2023-03-08 13:54:14 +0900

    1st commmit

--- src/view.py
+++ src/view.py
@@ -1,15 +1,12 @@
 # coding: utf-8
 
 #　必要なモジュールのインポート
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, request
 
 # app という変数でFlaskオブジェクトをインスタンス化
 app = Flask(__name__)
 
-# --- View側の設定 ---
-# ルートディレクトリにアクセスした場合の挙動
-@app.route('/', methods=['GET', 'POST'])
-# def以下がアクセス後の操作
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     #return '3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P'
     #return render_template('index.html') #追加
@@ -18,4 +15,4 @@ def index():
     return response
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
