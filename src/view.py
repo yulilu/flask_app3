019d9898a8ee3b861d7ebad77a3da9ab26939967
ykoga <yokoga@gmail.com>
2023-03-08 19:13:49 +0900

    1st commmit

--- src/view.py
+++ src/view.py
@@ -1,21 +1,35 @@
 # coding: utf-8
 
 #　必要なモジュールのインポート
-from flask import Flask, render_template, make_response, request, jsonify
+from flask import Flask, render_template, make_response, requests, jsonify
 
 # app という変数でFlaskオブジェクトをインスタンス化
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    # Slackから送られてくるPOSTリクエストのBodyの内容を取得
+    '''
     json =  request.json
     print(json)
-    # レスポンス用のJSONデータを作成
-    # 受け取ったchallengeのKey/Valueをそのまま返却する
     d = {'challenge' : json["challenge"]}
-    # レスポンスとしてJSON化して返却
     return jsonify(d)
+    '''
+    url = "https://slack.com/api/chat.postMessage"
+    token = "xoxb-3967341434739-4908753847316-hGvmqsRA7VTd3seUuGbHzq0E"# tokenを入れてください
+
+    header={
+        "Authorization": "Bearer {}".format(token)
+    }
+
+    data  = {
+        "channel" : "C04SXDS7N2W",# Conversation IDを入れてください
+        "text" : "Hello World!"
+        }
+
+    res = requests.post(url, headers=header, json=data)
+
+    resonse_json = res.json()
+    print(resonse_json)
 
 if __name__ == '__main__':
     app.run(debug=True)
