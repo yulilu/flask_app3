7db2d80198fa37af3e6398d1bc0097efa1802964
ykoga <yokoga@gmail.com>
2023-03-08 11:05:25 +0900

    1st commmit

--- src/view.py
+++ src/view.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 #　必要なモジュールのインポート
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 # app という変数でFlaskオブジェクトをインスタンス化
 app = Flask(__name__)
@@ -11,8 +11,11 @@ app = Flask(__name__)
 @app.route('/')
 # def以下がアクセス後の操作
 def index():
-    return '3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P'
+    #return '3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P'
     #return render_template('index.html') #追加
+    response = make_response('3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P')
+    response.headers['Content-type'] = 'text/plain'
+    return response
 
 if __name__ == '__main__':
     app.run()
