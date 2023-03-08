16d7fa44a00797415fb3886bb0076d7149e70221
ykoga <yokoga@gmail.com>
2023-03-08 22:52:14 +0900

    1

--- src/view.py
+++ src/view.py
@@ -6,7 +6,7 @@ import requests
 import json
 from slackeventsapi import SlackEventAdapter
 SLACK_SIGNING_SECRET = '6f1a03ac213789637ea8b8169c998487'
-SLACK_BOT_TOKEN = 'xoxb-3967341434739-4908753847316-kLUgpNeuPOdtMbbErULnE3DS'
+SLACK_BOT_TOKEN = 'xoxb-3967341434739-4908753847316-VVFayB6NRmzzYe0Fi2iW8zqH'
 
 
 import logging
