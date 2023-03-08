6374bdf6f96f0324f53b0322c1c1635dd58e4099
ykoga <yokoga@gmail.com>
2023-03-08 19:45:07 +0900

    1st commmit

--- src/view.py
+++ src/view.py
@@ -1,17 +1,30 @@
 # coding: utf-8
 
 #　必要なモジュールのインポート
-from flask import Flask, render_template, make_response, jsonify, request
+from flask import Flask, render_template, make_response, jsonify, request, Response
 import requests
+import json
 
 # app という変数でFlaskオブジェクトをインスタンス化
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    json =  request.json
-    return json
-
+    data = request.data.decode('utf-8')
+    data = json.loads(data)
+    # for challenge of slack api
+    if 'challenge' in data:
+        token = str(data['challenge'])
+        return Response(token, mimetype='text/plane')
+    # for events which you added
+    if 'event' in data:
+        print("get event")
+        event = data['event']
+        if 'user' in event:
+            print("user = ", event["user"])
+        if "text" in event:
+            print("text = ", event["text"])
+    return Response("nothing", mimetype='text/plane')
     '''
     json =  request.json
     print(json)
